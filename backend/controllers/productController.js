3b63ff77524b966a9ac234f142f6fd92de3f6cfd
Shayant Sital <shayant@gmail.com>
2020-12-06 21:43:45 -0300

    moved backend logic to controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// @desc Fetch single products
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+module.exports = {
+  getProductById,
+  getProducts,
+};
