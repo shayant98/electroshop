4e0b55bfcae799cc2ead41797138c7a30fb626d6
Shayant Sital <shayant@gmail.com>
2020-12-12 17:45:29 -0300

    added user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,6 +27,14 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
