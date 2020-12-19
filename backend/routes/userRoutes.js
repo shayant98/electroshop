702b5e58add248f8e136161f51d482e62a6e02e8
Shayant Sital <shayant@gmail.com>
2020-12-19 19:26:15 -0300

    profile & updateprofile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ const {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 module.exports = router;
