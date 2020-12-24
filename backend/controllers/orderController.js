f7c56299b604a3c4f540f95e90b2627559e8deab
Shayant Sital <shayant@gmail.com>
2020-12-24 01:38:48 -0300

    added get order route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,6 +36,24 @@ const addOrderItems = asyncHandler(async (req, res, next) => {
   }
 });
 
+// @desc Get order by id
+// @route Get /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 module.exports = {
   addOrderItems,
+  getOrderById,
 };
