947b26f077065e01fa060e5cd9e5fbba2345891f
Shayant Sital <shayant@gmail.com>
2020-12-29 16:55:56 -0300

    added procFile

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ connectDB();
 
 const PORT = 5000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -32,6 +28,19 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
