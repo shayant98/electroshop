74cd371f54bb952989ce4491077b66d7aece34e0
Shayant Sital <shayant@gmail.com>
2021-02-12 22:36:20 -0300

    fixed bug deliverdAt

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -144,7 +144,7 @@ const ProfileScreen = ({ history }) => {
                     </td>
                     <td>
                       {order.isDelivered ? (
-                        order.deliveredAt.substring(0, 10)
+                        order.deliveredAt?.substring(0, 10)
                       ) : (
                         <i className="fas fa-times" style={{ color: "red" }} />
                       )}
